ed79873ec648dadfe76d652c1bbeda38da602d9f
Hausdorff94 <jhonyal58@gmail.com>
2024-05-09 23:00:53 -0500

    Format imessages chat

--- src/feature_engineering/imesagges_preprocess.py
+++ src/feature_engineering/imesagges_preprocess.py
@@ -7,16 +7,24 @@ def load_data(chat_path: str):
 
 def clean_data(chat: list):
     # Unroll threads
-    pattern_thread = "^\t|    [A-Za-z]{3} \d{2}, \d{4}  \d{1,2}:\d{2}:\d{2} [AP]M"
-    unrolled_chat = []
+    # pattern_thread = "^\t|    [A-Za-z]{3} \d{2}, \d{4}  \d{1,2}:\d{2}:\d{2} [AP]M"
+    pattern_thread = "^\t|    .+"
+
     for i, line in enumerate(chat):
         if re.match(pattern_thread, line):
-            unrolled_chat.insert(i, '\n')
-        if line != '\n':
-            unrolled_chat.append(line.strip())
-        else:
-            unrolled_chat.append(line)
-    return unrolled_chat
+            chat.pop(i)
+
+
+    # unrolled_chat = [re.sub(pattern_thread, 'thread_delete_me', line).strip() for line in chat]
+    # for i, line in enumerate(chat):
+    #     if re.match(pattern_thread, line):
+    #         unrolled_chat.insert(i, '\n')
+    #     if line != '\n':
+    #         unrolled_chat.append(line.strip())
+    #     else:
+    #         unrolled_chat.append(line)
+    # chat = unrolled_chat
+    return chat
 
 if __name__ == '__main__':
     # Load data
