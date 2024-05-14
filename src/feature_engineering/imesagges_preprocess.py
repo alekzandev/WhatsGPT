bdec378befd70f507ba51ede8ef1537986c55490
Hausdorff94 <jhonyal58@gmail.com>
2024-05-13 23:05:38 -0500

    Clean iMessages

--- src/feature_engineering/imesagges_preprocess.py
+++ src/feature_engineering/imesagges_preprocess.py
@@ -9,13 +9,20 @@ def clean_data(chat: list):
     # Unroll threads
     # pattern_thread = "^\t|    [A-Za-z]{3} \d{2}, \d{4}  \d{1,2}:\d{2}:\d{2} [AP]M"
     pattern_thread = "^\t|    .+"
+    patterns_unrolled_message = "This message responded to an earlier message."
 
     for i, line in enumerate(chat):
         if re.match(pattern_thread, line):
-            chat.pop(i)
+            chat[i] = line.strip()
+
+        # Remove unrolled messages
+        if re.match(patterns_unrolled_message, line):
+            chat[i-1] = 'DELETE_ME: ' + chat[i-1]
+            chat[i-2] = 'DELETE_ME: ' + chat[i-2]
+            chat[i-3] = 'DELETE_ME: ' + chat[i-3]
 
 
-    # unrolled_chat = [re.sub(pattern_thread, 'thread_delete_me', line).strip() for line in chat]
+    #chat = [re.sub(pattern_thread, 'thread_delete_me', line).strip() for line in chat]
     # for i, line in enumerate(chat):
     #     if re.match(pattern_thread, line):
     #         unrolled_chat.insert(i, '\n')
@@ -30,8 +37,7 @@ if __name__ == '__main__':
     # Load data
     data_path = 'data/raw/_imessages-prod.txt'
     chat = load_data(data_path)
-    print(chat[178:190])
     # Clean data
     chat = clean_data(chat)
-    print()
-    print(chat[178:190])
+    for line in chat:
+        print(line)
